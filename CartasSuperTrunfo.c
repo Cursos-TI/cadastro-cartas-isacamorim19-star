70634b6ae482cb21e597b9995b1011dea7256dff
isacamorim19-star <isacamorim19@gmail.com>
2025-10-10 14:17:31 +0000

    atualizei.2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,18 +15,15 @@ int main() {
     return 0;
 }
 
-
-#include <stdio.h> 
-
 int main() {
     // Carta 1 - São Paulo
     char codigo1[4] = "A01";
     char nome_cidade1[50] = "São Paulo";
     int populacao1 = 12396372;
-    int area1 = 248222;
+    int area1 = 248222; 
     float pib1 = 2.32;
     int pontos_turisticos1 = 20;
-
+    
     // Carta 2 - Rio de Janeiro
     char codigo2[4] = "A02";
     char nome_cidade2[50] = "Rio de Janeiro";
@@ -40,7 +37,7 @@ int main() {
     printf("Código: %s\n", codigo1);
     printf("Cidade: %s\n", nome_cidade1);
     printf("População: %d habitantes\n", populacao1);
-    printf("Área: %d km²\n", area1);
+    printf("Área: %d km2\n", area1);
     printf("PIB: %.2f trilhões R$\n", pib1);
     printf("Pontos Turísticos: %d\n\n", pontos_turisticos1);
 
@@ -49,7 +46,7 @@ int main() {
     printf("Código: %s\n", codigo2);
     printf("Cidade: %s\n", nome_cidade2);
     printf("População: %d habitantes\n", populacao2);
-    printf("Área: %d km²\n", area2);
+    printf("Área: %d km2\n", area2);
     printf("PIB: %.2f trilhões R$\n", pib2);
     printf("Pontos Turísticos: %d\n", pontos_turisticos2);
 
